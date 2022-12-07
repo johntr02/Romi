a57c6e18feba6c8f5ac07f89f49e84c777082fbe
John Truong <itsmehelp02@gmail.com>
2022-12-06 21:11:01 -0800

    added averageDistanceInch

--- RomiDrivetrain.java
+++ RomiDrivetrain.java
@@ -60,7 +60,7 @@ public class RomiDrivetrain extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // This method will be called once per scheduler run.
   }
 
   @Override
